4e136389487325009ad92c6711d47073467e57c5
Stream Protocol Ltd <streamprotocol@protonmail.com>
2023-06-21 06:19:52 +0000

    fix

--- medusa-config.js
+++ medusa-config.js
@@ -2,95 +2,155 @@ const dotenv = require("dotenv");
 
 let ENV_FILE_NAME = "";
 switch (process.env.NODE_ENV) {
-  case "production":
-    ENV_FILE_NAME = ".env.production";
-    break;
-  case "staging":
-    ENV_FILE_NAME = ".env.staging";
-    break;
-  case "test":
-    ENV_FILE_NAME = ".env.test";
-    break;
-  case "development":
-  default:
-    ENV_FILE_NAME = ".env";
-    break;
+    case "production":
+        ENV_FILE_NAME = ".env.production";
+        break;
+    case "staging":
+        ENV_FILE_NAME = ".env.staging";
+        break;
+    case "test":
+        ENV_FILE_NAME = ".env.test";
+        break;
+    case "development":
+    default:
+        ENV_FILE_NAME = ".env";
+        break;
 }
 
 try {
-  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+    dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,https://admin.streampay.store";
+    process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,https://admin.streampay.store";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000,https://streampay.store";
 
-const DATABASE_URL = 
-  process.env.DATABASE_URL || "postgres://enonthqqzvbhvr:dfedf551af72fc1177752586756fdbc1550755872d6ee5c15fa6a68d2dfc4369@ec2-34-242-199-141.eu-west-1.compute.amazonaws.com:5432/d28br80p45qbge";
-
+const DATABASE_TYPE = process.env.DATABASE_TYPE || "postgres";
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://enonthqqzvbhvr:dfedf551af72fc1177752586756fdbc1550755872d6ee5c15fa6a68d2dfc4369@ec2-34-242-199-141.eu-west-1.compute.amazonaws.com:5432/d28br80p45qbge";
 const REDIS_URL = process.env.REDIS_URL || "redis://default:JXxWvP1twsaS3LleLUubr48my3Ke2hBE7IX5cdvULQeTNsbrq8uCWjxQlzuYSda7@6qcck4.stackhero-network.com:6379";
 
+// Stripe keys
+const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "pk_sk_live_51N5Q0DKjdN5iZkcXDshrrkFzap7WanNdN4UIcWlB7Ux2QdeGSAjqKUVfa2Z6mKFYkVO6Ey7WiuKAyszOsR1qLfjK00FuI172wM";
+const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "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";
+
 const plugins = [
-  `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
-  {
-    resolve: `@medusajs/file-local`,
-    options: {
-      upload_dir: "uploads",
+    `medusa-fulfillment-manual`,
+    `medusa-payment-manual`,
+    // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
+    // Please note is not recommended to build the admin in production, cause a minimum of 2GB RAM
+    // is required.
+    {
+        resolve: "@medusajs/admin",
+        /** @type {import('@medusajs/admin').PluginOptions} */
+        options: {
+            autoRebuild: true,
+        },
     },
-  },
-  // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
-   {
-     resolve: "@medusajs/admin",
-     /** @type {import('@medusajs/admin').PluginOptions} */
-     options: {
-       autoRebuild: true,
-     },
-   },
-];
+    {
+        resolve: `medusa-payment-stripe`,
+        options: {
+            api_key: STRIPE_API_KEY,
+            webhook_secret: STRIPE_WEBHOOK_SECRET,
+            automatic_payment_methods: true,
+        },
+    },
+    {
+        resolve: `medusa-file-minio`,
+        options: {
+            endpoint: process.env.MINIO_ENDPOINT,
+            bucket: process.env.MINIO_BUCKET,
+            access_key_id: process.env.MINIO_ACCESS_KEY,
+            secret_access_key: process.env.MINIO_SECRET_KEY,
+        },
+    },
+    {
+        resolve: `medusa-plugin-sendgrid`,
+        options: {
+            api_key: process.env.SENDGRID_API_KEY,
+            from: process.env.SENDGRID_FROM,
+            order_placed_template: process.env.SENDGRID_ORDER_PLACED_ID,
+            localization: {
+                "en-EN": { // locale key
+                    order_placed_template: process.env.SENDGRID_ORDER_PLACED_ID_LOCALIZED,
+          },
+    },
+    {
+        resolve: `medusa-plugin-meilisearch`,
+        options: {
+            config: {
+                host: process.env.MEILISEARCH_HOST,
+                apiKey: process.env.MEILISEARCH_API_KEY,
+            },
+            settings: {
+                // index name
+                products: {
+                    indexSettings: {
+                        searchableAttributes: [
+                            "title",
+                            "description",
+                            "variant_sku",
+                        ],
+                        displayedAttributes: [
+                            "title",
+                            "description",
+                            "variant_sku",
+                            "thumbnail",
+                            "handle",
+                        ],
+                    },
+                    primaryKey: "id",
+                    transform: (product) => ({
+                        id: product.id,
+                        // other attributes...
+         },
+    },
+    {
 
 const modules = {
-  /*eventBus: {
-    resolve: "@medusajs/event-bus-redis",
-    options: {
-      redisUrl: REDIS_URL
-    }
-  },
-  cacheService: {
-    resolve: "@medusajs/cache-redis",
-    options: {
-      redisUrl: REDIS_URL
-    }
-  },*/
-};
+    eventBus: {
+        resolve: "@medusajs/event-bus-redis",
+        options: {
+            redisUrl: REDIS_URL
+        }
+    },
+    cacheService: {
+        resolve: "@medusajs/cache-redis",
+        options: {
+            redisUrl: REDIS_URL
+        }
+    },
+}
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
-  jwtSecret: process.env.JWT_SECRET,
-  cookieSecret: process.env.COOKIE_SECRET,
-  store_cors: STORE_CORS,
-  database_url: DATABASE_URL,
-  admin_cors: ADMIN_CORS,
-  // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL
-};
+    jwtSecret: process.env.JWT_SECRET,
+    cookieSecret: process.env.COOKIE_SECRET,
+    database_database: "./medusa-db.sql",
+    database_type: DATABASE_TYPE,
+    store_cors: STORE_CORS,
+    admin_cors: ADMIN_CORS,
+    // Uncomment the following lines to enable REDIS
+    redis_url: REDIS_URL
+}
+
+if (DATABASE_URL && DATABASE_TYPE === "postgres") {
+    projectConfig.database_url = DATABASE_URL;
+    delete projectConfig["database_database"];
+}
+
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
-  projectConfig: {
-    redis_url: REDIS_URL,
-    database_url: DATABASE_URL,
-    database_type: "postgres",
-    store_cors: STORE_CORS,
-    admin_cors: ADMIN_CORS,
-    database_extra:
-      process.env.NODE_ENV !== "development"
-        ? { ssl: { rejectUnauthorized: false } }
-        : {},
-  },
-  plugins,
-  modules,
+    projectConfig: {
+        redis_url: REDIS_URL,
+        database_url: DATABASE_URL,
+        database_type: "postgres",
+        store_cors: STORE_CORS,
+        admin_cors: ADMIN_CORS,
+        database_extra: process.env.NODE_ENV !== "development" ? { ssl: { rejectUnauthorized: false } } : {},
+    },
+    plugins,
 }
--- src/api/routes/store/index.ts
+++ src/api/routes/store/index.ts
@@ -1,14 +1,17 @@
-import { Router } from "express";
-import customRouteHandler from "./custom-route-handler";
+import * as cors from "cors"
+import { Router } from "express"
+import * as bodyParser from "body-parser"
+import customRouteHandler from "./custom-route-handler"
 import { wrapHandler } from "@medusajs/medusa";
 
-// Initialize a custom router
-const router = Router();
+const storeRouter = Router()
+export function getStoreRouter(storeCorsOptions): Router {
+  storeRouter.use(cors(storeCorsOptions), bodyParser.json())
 
-export function attachStoreRoutes(storeRouter: Router) {
-  // Attach our router to a custom path on the store router
-  storeRouter.use("/custom", router);
+  storeRouter.post(
+    "/store/my-custom-path",
+    wrapHandler(customRouteHandler)
+  )
 
-  // Define a GET endpoint on the root route of our custom path
-  router.get("/", wrapHandler(customRouteHandler));
+  return storeRouter
 }
