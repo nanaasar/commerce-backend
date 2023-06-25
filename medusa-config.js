354e1e195c19870f080c75abe510a038f7c0a037
StreamPay <streamprotocol@protonmail.com>
2023-06-25 09:49:19 +0300

    commit

---
+++ medusa-config.js
@@ -0,0 +1,90 @@
+const dotenv = require("dotenv");
+
+let ENV_FILE_NAME = "";
+switch (process.env.NODE_ENV) {
+    case "production":
+        ENV_FILE_NAME = ".env.production";
+        break;
+    case "staging":
+        ENV_FILE_NAME = ".env.staging";
+        break;
+    case "test":
+        ENV_FILE_NAME = ".env.test";
+        break;
+    case "development":
+    default:
+        ENV_FILE_NAME = ".env";
+        break;
+}
+
+try {
+    dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
+
+// CORS when consuming Medusa from admin
+const ADMIN_CORS =
+    process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+
+// CORS to avoid issues when consuming Medusa from a client
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+
+const DATABASE_URL =
+    process.env.DATABASE_URL || "postgres://StreamPay03:aW22y5k2@localhost:5432/medusa-store";
+
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
+// Stripe keys
+const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "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";
+const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "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";
+
+const plugins = [
+    `medusa-fulfillment-manual`,
+    `medusa-payment-manual`,
+    {
+        resolve: `@medusajs/file-local`,
+        options: {
+            upload_dir: "uploads",
+        },
+    },
+    // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
+    {
+        resolve: "@medusajs/admin",
+        /** @type {import('@medusajs/admin').PluginOptions} */
+        options: {
+            autoRebuild: true,
+        },
+    },
+];
+
+const modules = {
+    /*eventBus: {
+      resolve: "@medusajs/event-bus-redis",
+      options: {
+        redisUrl: REDIS_URL
+      }
+    },
+    cacheService: {
+      resolve: "@medusajs/cache-redis",
+      options: {
+        redisUrl: REDIS_URL
+      }
+    },*/
+};
+
+/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
+const projectConfig = {
+    jwtSecret: process.env.JWT_SECRET,
+    cookieSecret: process.env.COOKIE_SECRET,
+    store_cors: STORE_CORS,
+    database_url: DATABASE_URL,
+    admin_cors: ADMIN_CORS,
+    // Uncomment the following lines to enable REDIS
+    // redis_url: REDIS_URL
+};
+
+/** @type {import('@medusajs/medusa').ConfigModule} */
+module.exports = {
+    projectConfig,
+    plugins,
+    modules,
+};
